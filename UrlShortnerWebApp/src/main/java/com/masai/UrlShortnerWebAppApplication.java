0ffefaf0dd40084bdbc0687e52ebbcd2142b6ebb
Nitish Kumar <103942404+nitish906@users.noreply.github.com>
2022-10-26 15:24:44 +0530

    Url

---
+++ UrlShortnerWebApp/src/main/java/com/masai/UrlShortnerWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.masai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShortnerWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlShortnerWebAppApplication.class, args);
+	}
+
+}
